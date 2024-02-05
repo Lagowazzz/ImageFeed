99d7b4f56d43c408847c83f0128972e1f9cba249
Lagowazzz <lagowazzz@mail.ru>
2024-02-05 18:23:21 +0300

    dateFormatter was created func addGradient was upgrade tableView added more cells with image

--- ImageFeed/IImageList/ImageListCell.swift
+++ ImageFeed/IImageList/ImageListCell.swift
@@ -8,15 +8,4 @@ final class ImageListCell: UITableViewCell {
     @IBOutlet weak var imageViewCell: UIImageView!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var likeButton: UIButton!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        
-        let gradientLayer = CAGradientLayer()
-        gradientLayer.frame = CGRect(x: 0, y: imageViewCell.bounds.height - (-40), width: imageViewCell.bounds.width, height: 40)
-        gradientLayer.colors = [UIColor.black.withAlphaComponent(0.0).cgColor, UIColor.black.withAlphaComponent(0.2).cgColor]
-        let gradientEndPoint = Float(50) / Float(imageViewCell.bounds.height)
-        gradientLayer.locations = [0.0, NSNumber(value: gradientEndPoint)]
-        imageViewCell.layer.addSublayer(gradientLayer)
-    }
 }
--- ImageFeed/IImageList/ImageListViewController.swift
+++ ImageFeed/IImageList/ImageListViewController.swift
@@ -2,26 +2,44 @@
 import UIKit
 
 class ImageListViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
     }
-
+    
     @IBOutlet private var tableView: UITableView!
     
-    func configCell(for cell: ImageListCell){}
+    private lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        return formatter
+    }()
     
+    func configCell(for cell: ImageListCell, with indexPath: IndexPath){
+        guard let image = UIImage(named: photosName[indexPath.row]) else {
+            return
+        }
+        cell.imageViewCell.image = image
+        cell.dateLabel.text = dateFormatter.string(from: Date())
+        
+        let isLiked = indexPath.row % 2 == 0
+        let likeImage = isLiked ? UIImage(named: "Active") : UIImage(named: "NoActive")
+        cell.likeButton.setImage(likeImage, for: .normal)
+    }
+    
+    private let photosName: [String] = Array(0..<20).map{"\($0)"}
 }
 
 extension ImageListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
     }
 }
 
 extension ImageListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return photosName.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -29,8 +47,32 @@ extension ImageListViewController: UITableViewDataSource {
         guard let imageListCell = cell as? ImageListCell else {
             return UITableViewCell()
         }
-        configCell(for: imageListCell)
+        configCell(for: imageListCell, with: indexPath)
+        addGradient(to: imageListCell)
         return imageListCell
+    }
+    
+    private func addGradient(to imageViewCell: ImageListCell) {
+        imageViewCell.layer.sublayers?.filter { $0 is CAGradientLayer }.forEach { $0.removeFromSuperlayer() }
         
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.cornerRadius = 16
+        gradientLayer.frame = CGRect(x: 16, y: imageViewCell.bounds.height - 33, width: imageViewCell.bounds.width - 32, height: 30)
+        gradientLayer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+        gradientLayer.colors = [UIColor.clear.cgColor, UIColor(red: 26/255, green: 27/255, blue: 34/255, alpha: 1).cgColor]
+        gradientLayer.locations = [0, 1]
+        imageViewCell.layer.addSublayer(gradientLayer)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        guard let image = UIImage(named: photosName[indexPath.row]) else {
+            return 0
+        }
+        let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
+        let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
+        let imageWidth = image.size.width
+        let scale = imageViewWidth / imageWidth
+        let cellHeight = image.size.height * scale + imageInsets.top + imageInsets.bottom
+        return cellHeight
     }
 }
