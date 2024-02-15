627ef3e4d410ed8fc76aee7cf22df3a3cb849115
Lagowazzz <lagowazzz@mail.ru>
2024-02-16 02:17:20 +0300

    Profile/ProfileVC was created

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,20 @@
+
+import Foundation
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var avatarImageButton: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var loginNameLabel: UILabel!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    @IBOutlet weak var logoutButton: UIButton!
+    
+    @IBAction func didTapLogoutButton(_ sender: Any) {
+    }
+    
+}
