e33c5797fcf8c79fcdd5f80537acf2a5502417e0
Lagowazzz <lagowazzz@mail.ru>
2024-04-22 18:36:04 +0300

    UI tests are complete

--- ImageFeedUITests/ImageFeedUITests.swift
+++ ImageFeedUITests/ImageFeedUITests.swift
@@ -2,10 +2,10 @@ import XCTest
 
 final class ImageFeedUITests: XCTestCase {
     private let app = XCUIApplication()
-    let yourEmail = ""
-    let yourPassword = ""
-    let yourFullName = ""
-    let yourUserName = ""
+    let yourEmail = "Lagowazzz@mail.ru"
+    let yourPassword = "Praktikum12345"
+    let yourFullName = "Ilya Lyashenko"
+    let yourUserName = "@lagowazzz"
     
     override func setUpWithError() throws {
         continueAfterFailure = false
@@ -39,38 +39,44 @@ final class ImageFeedUITests: XCTestCase {
     
     func testFeed() throws {
         let tablesQuery = app.tables
-
+        
         let cell = tablesQuery.children(matching: .cell).element(boundBy: 0)
         cell.swipeUp()
-
+        
         sleep(2)
-
+        
         let cellToLike = tablesQuery.children(matching: .cell).element(boundBy: 1)
-
+        
         cellToLike.buttons["Like"].tap()
         sleep(2)
         cellToLike.buttons["Like"].tap()
-
+        
         sleep(2)
+        
         cellToLike.tap()
+        
         sleep(2)
-
+        
         let image = app.scrollViews.images.element(boundBy: 0)
         image.pinch(withScale: 3, velocity: 1)
         image.pinch(withScale: 0.5, velocity: -1)
-
+        
         let navBackButtonWhiteButton = app.buttons["nav back button white"]
         navBackButtonWhiteButton.tap()
     }
+
+
+
+
     
     func testProfile() throws {
         app.tabBars.buttons.element(boundBy: 1).tap()
-        
+        sleep(2)
         XCTAssertTrue(app.staticTexts[yourFullName].exists)
         XCTAssertTrue(app.staticTexts[yourUserName].exists)
         
         app.buttons["logout button"].tap()
-        
+        sleep(2)
         app.alerts["Пока, пока!"].scrollViews.otherElements.buttons["Да"].tap()
     }
 }
